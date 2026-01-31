87ffd72317c09336fa27c5b229cf11339add8c2c
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-01-31 23:51:59 +0530

    Synchronize existing submission history (3/3)

---
+++ math/water-bottles-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        total=numBottles
+        while(numBottles>=numExchange):
+            numBottles-=numExchange-1
+            numExchange+=1
+            total+=1
+        return total
