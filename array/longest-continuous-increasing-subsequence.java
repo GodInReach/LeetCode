f28324a1c87d9a6a18ebbf6e37c5fa7d6bc0bf48
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-03-16 23:38:39 +0530

    LC-longest-continuous-increasing-subsequence [Runtime: 2 ms; Memory: 47.2 MB]

--- array/longest-continuous-increasing-subsequence.java
+++ array/longest-continuous-increasing-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
-        int max = 0;
+        int max = 1;
         int now = 1;
         for(int i = 1; i<nums.length; i++){
             if(nums[i-1]<nums[i]){
