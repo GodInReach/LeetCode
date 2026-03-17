1c5632752b8aaa240d3382a79ecc7c35a762b96f
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-03-17 21:33:39 +0530

    LC-longest-harmonious-subsequence [Runtime: 17 ms; Memory: 48.6 MB]

--- sorting/longest-harmonious-subsequence.java
+++ sorting/longest-harmonious-subsequence.java
@@ -1,23 +1,16 @@
 class Solution {
     public int findLHS(int[] nums) {
         Arrays.sort(nums);
-        int count = 0;
         int maxx = 0;
-        if(nums.length==1 || nums[0]==nums[nums.length-1]){
-            return 0;
-        }
         int j = 0;
-        for (int i = 1; i<nums.length; i++){
-            if(nums[i]-nums[j]==1){
-                count++;
-                maxx++;
-            }
-            else{
-                maxx=Math.max(count,maxx);
-                count=0;
+        for (int i = 0; i<nums.length; i++){
+            while(nums[i]-nums[j]>1){
                 j++;
             }
+            if(nums[i]-nums[j]==1){
+                maxx=Math.max(maxx,i-j+1);
+            }
         }
-        return maxx+1;
+        return maxx;
     }
 }
