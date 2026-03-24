9182dcf872ac64313d6c37b1fcb83058c7d941a4
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-03-25 00:09:02 +0530

    LC-valid-anagram [Runtime: 3 ms; Memory: 46.4 MB]

--- string/valid-anagram.java
+++ string/valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if ( s.length() != t.length() ) return false;
-        
         char[] sc = s.toCharArray();
-        char[] tc = t.toCharArray();
-        
         Arrays.sort(sc);
+        char[] tc = t.toCharArray();
         Arrays.sort(tc);
-        
-        return Arrays.equals(sc,tc);
+        String stc = new String(sc);
+        String stt = new String(tc);
+        if (stc.equals(stt) )return true;
+        return false;
     }
 }
