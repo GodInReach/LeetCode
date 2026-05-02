9c16280c8dd0206bb9633c604055b011599873fc
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-05-03 00:42:06 +0530

    LC-find-the-highest-altitude [Runtime: 0 ms; Memory: 43.1 MB]

--- array/find-the-highest-altitude.java
+++ array/find-the-highest-altitude.java
@@ -2,7 +2,7 @@ class Solution {
     public int largestAltitude(int[] gain) {
         int[] alti = new int[gain.length+1];
         int maxi = 0;
-        for(int i = 1; i<gain.length; i++){
+        for(int i = 1; i<=gain.length; i++){
             alti[i]=alti[i-1]+gain[i-1];
             if(alti[i]>maxi){
                 maxi=alti[i];
