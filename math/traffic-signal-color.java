fc1f1455fb435ad3bb94d04738626e2c3ce65fc1
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-06-05 21:46:24 +0530

    LC-traffic-signal-color [Runtime: N/A; Memory: N/A]

--- math/traffic-signal-color.java
+++ math/traffic-signal-color.java
@@ -1,7 +1,7 @@
 class Solution {
     public String trafficSignal(int timer) {
         if(timer==0) return "Green";
-        else if(timer==0) return "Orange";
+        else if(timer==30) return "Orange";
         else if(30<timer && timer<=90) return "Red";
         else return "Invalid";
     }
