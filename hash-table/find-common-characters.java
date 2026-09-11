a277354a5b0275c921303cedbb34ddc1821b9c9f
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-09-11 12:01:34 +0530

    LC-find-common-characters [Runtime: N/A; Memory: N/A]

--- hash-table/find-common-characters.java
+++ hash-table/find-common-characters.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> commonChars(String[] words) {
         ArrayList<String> li = new ArrayList<>();
         int[] comm = new int[26];
-        Arrays.fill(comm, 50);
+        Arrays.fill(comm, 100);
         for(String s : words){
             int[] chars = new int[26];
             for(int i = 0; i<s.length(); i++){
