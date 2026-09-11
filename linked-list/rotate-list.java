ec3125f65fdbebb81b9d78d7649a05b17f6f3574
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-09-11 10:10:00 +0530

    LC-rotate-list [Runtime: N/A; Memory: N/A]

--- linked-list/rotate-list.java
+++ linked-list/rotate-list.java
@@ -10,6 +10,9 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if (head == null){
+            return head;
+        }
         int n = 0;
         ListNode temp = head;
         while(temp.next!=null){
