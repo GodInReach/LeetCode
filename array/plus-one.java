781bdf20c8314de6795271d51d8c7a9c1295431d
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-09-11 10:59:54 +0530

    LC-plus-one [Runtime: N/A; Memory: N/A]

--- array/plus-one.java
+++ array/plus-one.java
@@ -4,7 +4,7 @@ class Solution {
         int[] op = new int[len+1];
         int rem = 1;
         for(int i = len; i>0; i--){
-            if(dig[i-1]==9){
+            if(dig[i-1]==9 && rem != 0){
                 op[i]=0;
                 rem = 1;
             }
