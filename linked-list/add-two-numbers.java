9ae5e86dba706875eacc510490b6cc6fcfcb105a
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-09-12 10:06:38 +0530

    LC-add-two-numbers [Runtime: N/A; Memory: N/A]

--- linked-list/add-two-numbers.java
+++ linked-list/add-two-numbers.java
@@ -1,20 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode op = new ListNode();
         ListNode head = op;
         int rem = 0;
         while(l1!=null || l2!=null){
-            int a = (l1!=null) ? l1.v…  sum-=10;
+            int a = (l1!=null) ? l1.val : 0;
+            int b = (l2!=null) ? l2.val : 0;
+            int sum = a+b+rem;
+            if(sum>9){
+                sum-=10;
                 rem = 1;
             }
             else{
