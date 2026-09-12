cc3e55a1f7e76ba661b3fbab9a76ec10a387e693
Giridharan S <95154586+GodInReach@users.noreply.github.com>
2026-09-12 12:45:07 +0530

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

--- array/remove-duplicates-from-sorted-array.java
+++ array/remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int[] visit = new int[201];
+        int[] visit = new int[202];
         int count = 0;
         for(int i = 0; i<nums.length; i++){
             if(visit[nums[i]+100]!=0){
